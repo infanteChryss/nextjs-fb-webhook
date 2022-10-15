839cdce04369e1d947ba06cb3c4c47102de343f1
Chryss Infante <christianreyinfante@gmail.com>
2022-10-16 05:40:41 +0800

    add webhook 404

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
             res.sendStatus(403);      
           }
         } else {
-          res.status(200).send("No Params Received");
+          res.status(404).send("No Params Received");
         }
       break;
   }
